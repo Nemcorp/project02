b5ee0b11520776205c8a1b74b3ce5c40c26fd3b5
Nicko <nwmariano@gmail.com>
2020-08-28 16:37:29 -0400

    fix a bug that was causing the wrong students to display. The issue was in generateNewStudenList(), where we needed to use the local variable as an argument for addPagination() and showPage()

--- js/script.js
+++ js/script.js
@@ -16,17 +16,34 @@ const itemsPerPage = 9;
 var studentList = data;
 
 
+
 // Call functions
+init();
 
+/**
+* Initializes page by adding the students, pagination, and a search bar.
+*/
+function init() {
+	showPage(studentList, 1);
+	addPagination(studentList);
+	addSearchBar();
+}
 
-showPage(studentList, 1);
-addPagination(studentList);
-addSearchBar();
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to 
 display a "page" of nine students
+*/
+
+/**
+* Creates and adds the dom elements for students on a chosen page. Will display
+* "No Results Found" if list is <1
+*
+* @param {array} list - The complete array of student objects we will select
+*						from to populate the page. 
+* @param {int} 	 page - The page number that we want to display. (EG page == 2
+*						will display students 9 - 17, given 9 itemsPerPage)
 */
 function showPage(list, page) {
 	let startIndex = (page-1) * itemsPerPage;
@@ -44,7 +61,7 @@ function showPage(list, page) {
 	studentList.innerHTML = "";
 
 	// if no students are found, display "No students found"
-	if(list.length == 0){
+	if(list.length < 1){
 		studentList.innerHTML = `
 			<span>No results found</span>
 		`;
@@ -71,10 +88,11 @@ function showPage(list, page) {
 	}
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements 
-needed for the pagination buttons
+/**
+* Adds pagination buttons to the bottom of the page
+*
+* @param {array} list - The complete array of student objects we wish to 
+*						include for a given search.
 */
 function addPagination(list) {
 	var numPaginationButtons = Math.ceil(list.length/itemsPerPage);
@@ -116,7 +134,7 @@ function addPagination(list) {
 }
 
 /**
-* adds search bar to the header of the page
+* Adds search bar to the header of the page. Adds an event handler for search functionality.
 */
 function addSearchBar(){
 	const header = document.querySelector("header");
@@ -133,31 +151,34 @@ function addSearchBar(){
 }
 
 /**
-* 
+* Adds search logic to the search bar, generating a new list of student objects
+* for each click or keyup event.
 */
 function addSearchBarEventHandler(){
 	const searchButton = document.querySelector("header button");
 	searchButton.addEventListener("click", ()=>{
 		studentList = generateNewStudentList();
-		addPagination(studentList);
-		showPage(studentList,1);
 	});
 
 	const input = document.querySelector("#search");
 	input.addEventListener("keyup", ()=> {
 		studentList = generateNewStudentList();
-		addPagination(studentList);
-		showPage(studentList,1);
 	});
 }
 
+
 /**
-* 
+* Generates a list of new students based on the value typed into the input field. Creates
+* new pagination and shows new results
+*
+* @return {array} The list of student objects whose full name includes the string typed
+* 				  into the input field, case insensitive
 */
 function generateNewStudentList() {
 	let newStudentList = [];
 	const input = document.querySelector("#search");
-	let desiredLetters = input.value;
+	let desiredLetters = input.value.toLowerCase();
+	
 	// loop through data list to find matches
 	for(student of data){
 		if(studentNameIncludes(student, desiredLetters)){
@@ -165,19 +186,26 @@ function generateNewStudentList() {
 		}
 	}
 
+	addPagination(newStudentList);
+	showPage(newStudentList,1);
+
 	return newStudentList;
 }
 
-
+/**
+* Determines if a student's name contains the desiredLetters. 
+*
+* @param {Object} student         The student whose name we will search for a substring
+* @param {str} 	  desiredLetters  The substring which we will search for in the student's name.
+*								  Must take lowercase letters.
+*
+* @return {boolean} True if student's full name includes 'desiredLetters'. False otherwise.
+* 				    
+*/
 function studentNameIncludes(student, desiredLetters) {
 	let studentName = student.name.first + " " + student.name.last;
 	return(studentName.toLowerCase().includes(desiredLetters));
 }
-
-
-
-
-
 
 
 
